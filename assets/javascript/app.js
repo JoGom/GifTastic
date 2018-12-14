4069ab303d0d4a7e991cf65cc67f6d08a05a714f
JoGom <jogoram428@gmail.com>
2018-12-14 00:09:35 -0700

    added function that displays the GIFS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 var search = ["animals", "mood", "celebrity" ];
-console.log(search);
-renderBtn();
+
 function renderBtn(){
     $("#button-view").empty();
 
@@ -11,3 +10,31 @@ function renderBtn(){
         $("#button-view").append(btn);
     }
 }
+
+function displayGif(){
+
+    //grabs the name of the gif and stores it in a variable
+    var gif = "happy"//$(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+gif+"&api_key=kXT4Ft2jfr0GHTless9b5cYCQaAyGtqi&limit=10";
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+
+        for(var i = 0; i < response.data.length; i++){
+            var gifDiv = $("<div>");
+            var rating = $("<p>").text("rating: " + response.data[i].rating);
+
+            var gifImage = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+            console.log(response.data[i].images.fixed_height_still.url);
+            gifImage.attr("data-still", response.data[i].images.fixed_height_still.url).attr("data-animate", response.data[i].images.fixed_height.url).attr("state", "still");
+
+            gifDiv.append(gifImage);
+            gifDiv.append(rating);
+            $("#gif-view").prepend(gifDiv);
+        }
+    });
+}
+displayGif();
