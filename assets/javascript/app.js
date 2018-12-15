dca00641f5f1ba532213ee429bd085a1b2f5a8f3
JoGom <jogoram428@gmail.com>
2018-12-14 18:06:04 -0700

    added option for user to add buttons and for user to click the gif and have it play

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
+
 var search = ["animals", "mood", "celebrity" ];
+
+
 
 function renderBtn(){
     $("#button-view").empty();
@@ -6,18 +9,30 @@ function renderBtn(){
     for(var i = 0; i < search.length; i++){
         var btn = $("<button>");
         btn.attr("data-name", search[i]);
+        btn.addClass("gifBtn");
         btn.text(search[i]);
         $("#button-view").append(btn);
     }
 }
 
+//adds 10 button when they click the button
+$("#add-gif").on("click", function(event){
+    event.preventDefault();
+
+    var newGif = $("#gif-input").val().trim();
+
+    search.push(newGif);
+    renderBtn();
+});
+
 function displayGif(){
 
     //grabs the name of the gif and stores it in a variable
-    var gif = "happy"//$(this).attr("data-name");
+    var gif = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="+gif+"&api_key=kXT4Ft2jfr0GHTless9b5cYCQaAyGtqi&limit=10";
     console.log(queryURL);
 
+    $("#gif-view").empty();
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -28,13 +43,31 @@ function displayGif(){
             var rating = $("<p>").text("rating: " + response.data[i].rating);
 
             var gifImage = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
-            console.log(response.data[i].images.fixed_height_still.url);
-            gifImage.attr("data-still", response.data[i].images.fixed_height_still.url).attr("data-animate", response.data[i].images.fixed_height.url).attr("state", "still");
+         //   console.log(response.data[i].images.fixed_height_still.url);
+            gifImage.addClass("gifImg");
+            gifImage.attr("data-still", response.data[i].images.fixed_height_still.url).attr("data-animate", response.data[i].images.fixed_height.url).attr("data-state", "still");
 
+            gifDiv.addClass("gifContainer");
             gifDiv.append(gifImage);
             gifDiv.append(rating);
             $("#gif-view").prepend(gifDiv);
         }
     });
 }
-displayGif();
+
+$(document).on("click", ".gifImg", function(){
+
+    var state = $(this).attr("data-state");
+    console.log("hello");
+    if (state === "still"){
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    }
+    else{
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
+$(document).on("click", ".gifBtn", displayGif);
+renderBtn();
